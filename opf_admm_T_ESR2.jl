13d61c779dcd6dad23ba908e71acadaaf2a506f7
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2021-11-03 10:43:36 +0100

    Fix mistake in objective function

--- opf_admm_T_ESR2.jl
+++ opf_admm_T_ESR2.jl
@@ -98,8 +98,8 @@ function subproblem_G_S(s,lambda, G_S_d_mean, G_S_d_old, G_S_c_mean, G_S_c_old, 
         sub,
         Min,
         sum(
-            mc[s] * (G_S_d[t] + G_S_d[t])
-            + lambda[t] * (G_S_d[t] + G_S_d[t]) 
+            mc[s] * (G_S_d[t] + G_S_c[t])
+            + lambda[t] * (G_S_d[t] + G_S_c[t]) 
             + gamma/2 * penalty_term[t]
             for t in T
         )
