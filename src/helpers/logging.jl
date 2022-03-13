934cda5e97d5ae88b23dece380c2474d89dfe746
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2022-03-13 13:14:17 +0100

    Optimize code style

--- src/helpers/logging.jl
+++ src/helpers/logging.jl
@@ -2,34 +2,34 @@ function print_results(print_generators::Bool,
     print_storages::Bool,
     print_penalty::Bool,
     iteration::Int)
-unit_to_result = admm.results[iteration].unit_to_result
 
-for (unit, result) in unit_to_result
-if typeof(unit) == Generator && print_generators
-print("$(result.generator.name): ")
-println(result.generation)
-elseif typeof(unit) == Storage && print_storages
-println("$(result.storage.name): ")
-println("\tdischarge: $(result.discharge)")
-println("\tcharge: $(result.charge)")
-println("\tlevel: $(result.level)")
-end
-end
-# TODO: Split between nodes and lines
-if print_penalty
-println("Penalty Sum per Node:")
-node_to_result = admm.results[iteration].node_to_result
-for (node, node_result) in node_to_result
-println("\t$(node.name): $(node_result.penalty_term)")
-end
-println("Penalty Sum Total: $(admm.results[iteration].penalty_term)")
-end
+    unit_to_result = admm.results[iteration].unit_to_result
+
+    for (unit, result) in unit_to_result
+        if typeof(unit) == Generator && print_generators
+            print("$(result.generator.name): ")
+            println(result.generation)
+        elseif typeof(unit) == Storage && print_storages
+            println("$(result.storage.name): ")
+            println("\tdischarge: $(result.discharge)")
+            println("\tcharge: $(result.charge)")
+            println("\tlevel: $(result.level)")
+        end
+    end
+    if print_penalty
+        println("Penalty Sum per Node:")
+        node_to_result = admm.results[iteration].node_to_result
+        for (node, node_result) in node_to_result
+            println("\t$(node.name): $(node_result.penalty_term)")
+        end
+        println("Penalty Sum Total: $(admm.results[iteration].penalty_term)")
+    end
 end
 
 function print_duals(iteration::Int)
-println(
-"\nlambdas: $(admm.lambdas[iteration])"
-* "\nmues: $(admm.mues[iteration])"
-* "\nrhos: $(admm.rhos[iteration])\n"
-)
+    println(
+        "\nlambdas: $(admm.lambdas[iteration])"
+        * "\nmues: $(admm.mues[iteration])"
+        * "\nrhos: $(admm.rhos[iteration])\n"
+    )
 end
