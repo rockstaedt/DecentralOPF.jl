8780b0bd1f6a5b5b0a900bf41aa9cb20ecc88f47
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2022-04-06 10:01:15 +0200

    Use difference for convergence check

--- src/optimization/convergence.jl
+++ src/optimization/convergence.jl
@@ -1,32 +1,14 @@
 function check_convergence!(admm::ADMM)
     eps = 10^(-3)
     if admm.iteration != 1
-        lambda_res = abs.(
-            (
-                admm.results[admm.iteration].generation
-                + admm.results[admm.iteration].discharge
-                - admm.results[admm.iteration].charge
-                - admm.total_demand
-            )
-        )
+        
+        lambda_res = abs.(admm.lambdas[end] - admm.lambdas[end-1])
         push!(admm.convergence.lambda_res, lambda_res)
 
-        mue_res = abs.(
-            (
-                admm.ptdf * admm.results[admm.iteration].injection
-                + admm.results[admm.iteration].avg_U
-                .- admm.f_max
-            )
-        )
+        mue_res = abs.(admm.mues[end] - admm.mues[end-1])
         push!(admm.convergence.mue_res, mue_res)
 
-        rho_res = abs.(
-            (
-                admm.results[admm.iteration].avg_K
-                - admm.ptdf * admm.results[admm.iteration].injection
-                .- admm.f_max
-            )
-        )
+        rho_res = abs.(admm.rhos[end] - admm.rhos[end-1])
         push!(admm.convergence.rho_res, rho_res)
 
         admm.convergence.lambda = all(lambda_res .< eps)
